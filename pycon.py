4d3c541d5b0c3cae8375776fc77fdaa5c794098c
viceren <viceren@live.com>
2017-05-12 13:28:50 +0800

    2 pycon commit

--- pycon.py
+++ pycon.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Viceren-Wang
 # @Date:   2017-05-11 16:49:42
-# @Last Modified by:   Viceren-Wang
-# @Last Modified time: 2017-05-12 13:15:35
+# @Last Modified by:   viceren
+# @Last Modified time: 2017-05-12 13:27:08
 
 
 class pycon(object):
@@ -11,11 +11,16 @@ class pycon(object):
 
     @property
     def partnum(self):
-        return self._partnum = partnum
+        return self._partnum
 
     @partnum.setter
     def partnum(self, partnum):
         self._partnum = partnum
 
-    def print_info():
-        pass
+    def print_info(self):
+        print("Part Number is defined as %s" % self.partnum)
+
+
+Ecase = pycon()
+Ecase.partnum = "80-A-001"
+Ecase.print_info()
