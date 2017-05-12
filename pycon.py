4ed21f1445a0face2ff7094f187b01f469daae5b
viceren <viceren@live.com>
2017-05-12 13:20:23 +0800

    pycon commit

---
+++ pycon.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: Viceren-Wang
+# @Date:   2017-05-11 16:49:42
+# @Last Modified by:   Viceren-Wang
+# @Last Modified time: 2017-05-12 13:15:35
+
+
+class pycon(object):
+    """docstring for pycon"""
+
+    @property
+    def partnum(self):
+        return self._partnum = partnum
+
+    @partnum.setter
+    def partnum(self, partnum):
+        self._partnum = partnum
+
+    def print_info():
+        pass
