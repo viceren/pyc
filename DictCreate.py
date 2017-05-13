1e1fbbe57649b092ee77a60a178416650f9356ba
viceren <viceren@live.com>
2017-05-13 11:33:02 +0800

    DictCreate commit

---
+++ DictCreate.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: viceren
+# @Date:   2017-05-13 11:29:17
+# @Last Modified by:   viceren
+# @Last Modified time: 2017-05-13 11:32:32
+
+
+class DictCreater(object):
+    """docstring for DictCreater"""
+
+    def __init__(self, arg):
+        self.arg = arg
